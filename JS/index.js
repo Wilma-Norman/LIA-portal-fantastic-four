1360c6ab4b531f24a0afb26d06e5be350559cf4b
Tim Deckel <Tim.Deckel@gmail.com>
2024-01-22 18:49:42 +0100

    made changes to index.html, css and js. and i have updated the root color so fix a spelling mistake and removed the css code for the class divider, i have also linked the buttons to the student and the company sites

---
+++ JS/index.js
@@ -0,0 +1,9 @@
+
+//simple js, just click on the targeted buttons and change the window location.
+$("#student-button").click(() => {
+    window.location.href = 'html/student-main-page.html';
+});
+
+$("#company-button").click(() => {
+    window.location.href = 'html/company-main-page.html';
+});
