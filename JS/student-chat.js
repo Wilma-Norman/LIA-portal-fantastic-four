04da81dffe047553b66bdfd8a14671e685c44b5f
bedirgcmz <bdrgcmz@gmail.com>
2024-01-24 00:20:41 +0100

    Student chat pages responsive css done

---
+++ JS/student-chat.js
@@ -0,0 +1,16 @@
+const icon = document.querySelector(".icon");
+
+function toggleModal() {
+  var modal = document.getElementById("myModal");
+  //   modal.style.display = modal.style.display === "none" ? "block" : "none";
+  modal.classList.toggle("none-dis");
+  console.log(modal);
+  toggleMenu();
+}
+
+icon.addEventListener("click", toggleModal);
+
+function toggleMenu() {
+  var icon = document.querySelector(".icon");
+  icon.classList.toggle("menu-open");
+}
