a22c6a564d68d7f8bba9e5ff3466410fa2ac017b
bedirgcmz <bdrgcmz@gmail.com>
2024-01-24 13:47:08 +0100

    Started work on student chat js

--- JS/student-chat.js
+++ JS/student-chat.js
@@ -1,10 +1,10 @@
+/* Hamburder menu START */
 const icon = document.querySelector(".icon");
 
 function toggleModal() {
   var modal = document.getElementById("myModal");
   //   modal.style.display = modal.style.display === "none" ? "block" : "none";
   modal.classList.toggle("none-dis");
-  console.log(modal);
   toggleMenu();
 }
 
@@ -14,3 +14,11 @@ function toggleMenu() {
   var icon = document.querySelector(".icon");
   icon.classList.toggle("menu-open");
 }
+/* Hamburder menu END */
+
+/* Render dynamic friends list to sidebar of student chat page  START*/
+console.log(students);
+//Dom elements
+const chatListContainer = document.querySelector(".chat-list-container");
+
+/* Render dynamic friends list to sidebar of student chat page  END*/
