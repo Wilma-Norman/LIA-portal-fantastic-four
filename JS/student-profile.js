03dfd2e585328bb159f23c4f1d06ef9d0c445fff
timdeckel <timdeckel@gmail.com>
2024-01-25 18:36:33 +0100

    fixed profile summary structure on web-mode and renamed css classes to be more semantic

--- JS/student-profile.js
+++ JS/student-profile.js
@@ -13,63 +13,68 @@ const createProfileElement = (profileId) => {
             <div class="profile-card-wrapper">
                 <div class="profile-card">
                     <div class="profile-summary flex-column-center">
-                    <img src="../Image/temp-avatar-icon.png" alt="profile icon">
-                    <p>${selectedStudent[0].name}</p>
-                    <div class="temp-link">
-                        <div class="temp-icon"></div> ${selectedStudent[0].contact.email}
+                        <img src="../Image/temp-avatar-icon.png" alt="profile icon">
+                    <div class="profile-summary-wrapper">
+                        <p>${selectedStudent[0].name}</p>
+                        <div class="project-link">
+                            <div class="link-icon">
+                                </div> ${selectedStudent[0].contact.email}
+                            </div>
+                        <div class="project-link">
+                            <div class="link-icon">
+                                </div> ${selectedStudent[0].contact.linkedIn}
+                            </div>  
+                        </div>
                     </div>
-                    <div class="temp-link">
-                        <div class="temp-icon"></div> ${selectedStudent[0].contact.linkedIn}
+                    <div class="profile-bio">
+                        <p class="title">Background</p>
+                        <p>Right now the fake link is absolutely positioned in the bottom right but i think a grid might work aswell, just put the link in the bottom right grid area and the rest can be text.</p>
+                        <div class="fake-link">fake link
+                        </div>
                     </div>
-                </div>
-                <div class="profile-bio">
-                    <p class="title">Background</p>
-                    <p>Right now the fake link is absolutely positioned in the bottom right but i think a grid might work aswell, just put the link in the bottom right grid area and the rest can be text.</p>
-                    <div class="fake-link">fake link</div>
-                </div>
                 </div>
                 <div class="link-container">
                     <p class="title">Projects</p>
-                    <div class="temp-link">
-                        <div class="temp-icon"></div> > Project with ${selectedStudent[0].skills[0]} 
+                    <div class="project-link">
+                        <div class="link-icon"></div> > Project with ${selectedStudent[0].skills[0]} 
                     </div>
-                    <div class="temp-link">
-                        <div class="temp-icon"></div> > Project with ${selectedStudent[0].skills[1]}
+                    <div class="project-link">
+                        <div class="link-icon"></div> > Project with ${selectedStudent[0].skills[1]}
                     </div>
-                    <div class="temp-link">
-                        <div class="temp-icon"></div> >
+                    <div class="project-link">
+                        <div class="link-icon"></div> >
                     </div>
                 </div>
                 <div class="link-container">
                     <p class="title">Resume</p>
-                    <div class="temp-link">
-                        <div class="temp-icon"></div> this is another link
+                    <div class="project-link">
+                        <div class="link-icon"></div> this is another link
                     </div>
                 </div>
             </div>
-            <div class="bob">
-                <div class="rob"></div>
+            <div class="divider-container">
+                <div class="divider"></div>
             </div>  
             <div class="container-text"> 
                 <p class="title">What am i looking for?</p>
-                <div class="temp-link">
-                    <div class="temp-icon"></div> sample text
+                <div class="project-link">
+                    <div class="link-icon"></div> sample text
                 </div>
-                <div class="temp-link">
-                    <div class="temp-icon"></div> sample text
+                <div class="project-link">
+                    <div class="link-icon"></div> sample text
                 </div>
-                <div class="temp-link">
-                    <div class="temp-icon"></div> sample text
+                <div class="project-link">
+                    <div class="link-icon"></div> sample text
                 </div>
                 <p class="title">What am i offering!</p>
-                <div class="temp-link">
-                    <div class="temp-icon"></div> ${selectedStudent[0].skills[0]}
+                <div class="project-link">
+                    <div class="link-icon"></div> ${selectedStudent[0].skills[0]}
                 </div>
-                <div class="temp-link">
-                    <div class="temp-icon"></div> ${selectedStudent[0].skills[1]}
+                <div class="project-link">
+                    <div class="link-icon"></div> ${selectedStudent[0].skills[1]}
                 </div>
-                <div class="temp-link">
-                    <div class="temp-icon"></div> something else
+                <div class="project-link">
+                    <div class="link-icon"></div> something else
                 </div>
             </div>
         </div>
