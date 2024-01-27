b64c95f92cc3c8450c33cdfb401465352654cdf5
bedirgcmz <bdrgcmz@gmail.com>
2024-01-27 20:38:43 +0100

    Has done render filter list

--- JS/student-main-page.js
+++ JS/student-main-page.js
@@ -1,4 +1,56 @@
-$(".temp-proff-button").click(function (){
-    console.log('clicky')
-});
+/* DOM elements */
+const filterWithSector = document.querySelector(".filter-with-sector");
+const filterWithCity = document.querySelector(".filter-with-city");
+const getFiltredList1Button = document.querySelector(".get-filtred-list-1");
+const getFiltredList2Button = document.querySelector(".get-filtred-list-2");
 
+let updateInternshipsdata = internships;
+const renderFilterList = () => {
+  // Create an array to hold all sector names
+  const allSectors = [];
+  const allCities = [];
+
+  // Iterate over each internship object
+  updateInternshipsdata.forEach((internship) => {
+    // Get the sector name of the internship
+    const sector = internship.companyInfo.sector;
+    const city = internship.city;
+
+    // If the sector name is not already in the allSectors array, add it
+    if (!allSectors.includes(sector)) {
+      allSectors.push(sector);
+    }
+    // If the city name is not already in the allSectors array, add it
+    if (!allCities.includes(city)) {
+      allCities.push(city);
+    }
+  });
+
+  console.log(allSectors);
+  console.log(allCities);
+
+  // Render the results to the console
+  filterWithSector.innerHTML += allSectors
+    .map((sector) => {
+      return `
+        <div>
+            <input type="checkbox" id="fullstack" name="${sector}" value="${sector}" />
+            <label for="${sector}">${sector}</label>
+        </div>
+        `;
+    })
+    .join("");
+
+  // Render the results to the console
+  filterWithCity.innerHTML += allCities
+    .map((city) => {
+      return `
+      <div>
+          <input type="checkbox" id="fullstack" name="${city}" value="${city}" />
+          <label for="${city}">${city}</label>
+      </div>
+      `;
+    })
+    .join("");
+};
+renderFilterList();
--- JS/testData.js
+++ JS/testData.js
@@ -255,3 +255,349 @@ students.push(
   studentNine,
   studentTen
 );
+
+///*Internships test data
+
+// Internship Constructor
+function Internship(
+  title,
+  companyName,
+  companyEmail,
+  city,
+  startDate,
+  endDate,
+  remoteWork,
+  requirements,
+  expectations,
+  companyInfo,
+  image
+) {
+  this.id = nextId++;
+  this.title = title;
+  this.companyName = companyName;
+  this.companyEmail = companyEmail;
+  this.city = city;
+  this.startDate = startDate;
+  this.endDate = endDate;
+  this.remoteWork = remoteWork;
+  this.requirements = requirements;
+  this.expectations = expectations;
+  this.companyInfo = companyInfo;
+  this.image = image;
+  this.chatHistory = [];
+  this.isActive = false;
+  this.receiver = false;
+}
+
+// Static variables for Internship constructor
+nextId = 100;
+cityIndex = 0;
+cities = ["Stockholm", "Malmo", "Sundsvall", "Goteborg"];
+
+// Creating 15 Internship Objects
+const internships = [];
+
+// Internship Objects
+const internship1 = new Internship(
+  "Software Developer Intern",
+  "Tech Solutions Inc.",
+  "info@techsolutions.com",
+  "Stockholm",
+  "2024-06-01",
+  "2024-08-31",
+  true,
+  ["Programming skills", "Team collaboration"],
+  "We expect the intern to actively participate in software development projects and collaborate with the team.",
+  {
+    establishmentYear: 2005,
+    numberOfEmployees: 150,
+    sector: "Software",
+    website: "https://www.techsolutions.com",
+  },
+  "path/to/image1.jpg"
+);
+
+const internship2 = new Internship(
+  "Marketing Intern",
+  "Marketing Trends Ltd.",
+  "info@marketingtrends.com",
+  "Stockholm",
+  "2024-07-01",
+  "2024-09-30",
+  false,
+  ["Social media knowledge", "Content creation"],
+  "We are looking for a creative marketing intern to help us with various marketing tasks.",
+  {
+    establishmentYear: 2010,
+    numberOfEmployees: 80,
+    sector: "Marketing",
+    website: "https://www.marketingtrends.com",
+  },
+  "path/to/image2.jpg"
+);
+
+const internship3 = new Internship(
+  "Data Science Intern",
+  "Data Insights AB",
+  "info@datainsights.com",
+  "Stockholm",
+  "2024-06-15",
+  "2024-09-15",
+  true,
+  ["Data analysis skills", "Statistical knowledge"],
+  "We seek a data science intern to work on various data analysis projects.",
+  {
+    establishmentYear: 2012,
+    numberOfEmployees: 120,
+    sector: "Data Science",
+    website: "https://www.datainsights.com",
+  },
+  "path/to/image3.jpg"
+);
+
+const internship4 = new Internship(
+  "Graphic Design Intern",
+  "Creative Designs Ltd.",
+  "info@creativedesigns.com",
+  "Stockholm",
+  "2024-07-01",
+  "2024-09-30",
+  false,
+  ["Adobe Creative Suite proficiency", "Creativity"],
+  "We are looking for a graphic design intern to assist in creating visually appealing content.",
+  {
+    establishmentYear: 2008,
+    numberOfEmployees: 90,
+    sector: "Design",
+    website: "https://www.creativedesigns.com",
+  },
+  "path/to/image4.jpg"
+);
+
+const internship5 = new Internship(
+  "Marketing Intern",
+  "Innovative Marketing Solutions",
+  "info@innovativemarketing.com",
+  "Malmo",
+  "2024-06-01",
+  "2024-08-31",
+  false,
+  ["Social media marketing", "Content creation"],
+  "We are searching for a marketing intern to assist in our innovative marketing campaigns.",
+  {
+    establishmentYear: 2010,
+    numberOfEmployees: 80,
+    sector: "Marketing",
+    website: "https://www.innovativemarketing.com",
+  },
+  "path/to/image5.jpg"
+);
+
+const internship6 = new Internship(
+  "Software Development Intern",
+  "CodeCrafters Inc.",
+  "info@codecrafters.com",
+  "Malmo",
+  "2024-07-01",
+  "2024-09-30",
+  true,
+  ["Programming skills", "Problem-solving abilities"],
+  "We are looking for a software development intern to join our coding team.",
+  {
+    establishmentYear: 2013,
+    numberOfEmployees: 150,
+    sector: "Software",
+    website: "https://www.codecrafters.com",
+  },
+  "path/to/image6.jpg"
+);
+
+const internship7 = new Internship(
+  "Finance Intern",
+  "MoneyMasters Ltd.",
+  "info@moneymasters.com",
+  "Malmo",
+  "2024-06-15",
+  "2024-08-15",
+  false,
+  ["Financial analysis skills", "Attention to detail"],
+  "We seek a finance intern to work on various financial projects and analyses.",
+  {
+    establishmentYear: 2011,
+    numberOfEmployees: 110,
+    sector: "Finance",
+    website: "https://www.moneymasters.com",
+  },
+  "path/to/image7.jpg"
+);
+
+const internship8 = new Internship(
+  "Event Management Intern",
+  "EventPros Organization",
+  "info@eventpros.com",
+  "Sundsvall",
+  "2024-07-15",
+  "2024-09-15",
+  true,
+  ["Organizational skills", "Creativity"],
+  "We are searching for an event management intern to assist in planning and coordinating events.",
+  {
+    establishmentYear: 2009,
+    numberOfEmployees: 95,
+    sector: "Event Management",
+    website: "https://www.eventpros.com",
+  },
+  "path/to/image8.jpg"
+);
+
+const internship9 = new Internship(
+  "Environmental Science Intern",
+  "EcoSolutions Corp.",
+  "info@ecosolutions.com",
+  "Sundsvall",
+  "2024-06-01",
+  "2024-08-31",
+  false,
+  ["Environmental research skills", "Sustainability knowledge"],
+  "We are seeking an environmental science intern to contribute to our eco-friendly projects.",
+  {
+    establishmentYear: 2014,
+    numberOfEmployees: 85,
+    sector: "Environmental Science",
+    website: "https://www.ecosolutions.com",
+  },
+  "path/to/image9.jpg"
+);
+
+const internship10 = new Internship(
+  "Customer Support Intern",
+  "SupportHub Ltd.",
+  "info@supporthub.com",
+  "Sundsvall",
+  "2024-07-01",
+  "2024-09-30",
+  true,
+  ["Communication skills", "Problem-solving abilities"],
+  "We are looking for a customer support intern to assist our clients and provide excellent service.",
+  {
+    establishmentYear: 2016,
+    numberOfEmployees: 120,
+    sector: "Customer Support",
+    website: "https://www.supporthub.com",
+  },
+  "path/to/image10.jpg"
+);
+
+const internship11 = new Internship(
+  "Fashion Design Intern",
+  "StyleCraft Fashion",
+  "info@stylecraft.com",
+  "Sundsvall",
+  "2024-06-15",
+  "2024-08-15",
+  false,
+  ["Fashion design skills", "Creativity"],
+  "We seek a fashion design intern to contribute to our latest fashion projects.",
+  {
+    establishmentYear: 2017,
+    numberOfEmployees: 100,
+    sector: "Fashion",
+    website: "https://www.stylecraft.com",
+  },
+  "path/to/image11.jpg"
+);
+
+const internship12 = new Internship(
+  "Healthcare Intern",
+  "WellnessCare Solutions",
+  "info@wellnesscare.com",
+  "Goteborg",
+  "2024-07-15",
+  "2024-09-15",
+  true,
+  ["Healthcare knowledge", "Empathy"],
+  "We are searching for a healthcare intern to support our wellness programs.",
+  {
+    establishmentYear: 2015,
+    numberOfEmployees: 90,
+    sector: "Healthcare",
+    website: "https://www.wellnesscare.com",
+  },
+  "path/to/image12.jpg"
+);
+
+const internship13 = new Internship(
+  "Education Intern",
+  "EduConnect Inc.",
+  "info@educonnect.com",
+  "Goteborg",
+  "2024-06-01",
+  "2024-08-31",
+  false,
+  ["Education background", "Teaching skills"],
+  "We are seeking an education intern to assist in educational projects and programs.",
+  {
+    establishmentYear: 2018,
+    numberOfEmployees: 110,
+    sector: "Education",
+    website: "https://www.educonnect.com",
+  },
+  "path/to/image13.jpg"
+);
+
+const internship14 = new Internship(
+  "Agriculture Intern",
+  "AgroTech Farms",
+  "info@agrotech.com",
+  "Goteborg",
+  "2024-07-01",
+  "2024-09-30",
+  true,
+  ["Agricultural knowledge", "Problem-solving abilities"],
+  "We are looking for an agriculture intern to contribute to our innovative farming practices.",
+  {
+    establishmentYear: 2019,
+    numberOfEmployees: 80,
+    sector: "Agriculture",
+    website: "https://www.agrotech.com",
+  },
+  "path/to/image14.jpg"
+);
+
+const internship15 = new Internship(
+  "Human Resources Intern",
+  "People Management Ltd.",
+  "info@peoplemanagement.com",
+  "Goteborg",
+  "2024-06-15",
+  "2024-08-15",
+  false,
+  ["Communication skills", "Recruitment knowledge"],
+  "We are seeking an HR intern to support our human resources department in various tasks.",
+  {
+    establishmentYear: 2015,
+    numberOfEmployees: 100,
+    sector: "Human Resources",
+    website: "https://www.peoplemanagement.com",
+  },
+  "path/to/image15.jpg"
+);
+
+internships.push(
+  internship1,
+  internship2,
+  internship3,
+  internship4,
+  internship5,
+  internship6,
+  internship7,
+  internship8,
+  internship9,
+  internship10,
+  internship11,
+  internship12,
+  internship13,
+  internship14,
+  internship15
+);
+console.log(internships);
